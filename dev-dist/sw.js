ef4252ed48887dbf7cc14d6ab5685febb68545fa
askidea <askinnovativelab@gmail.com>
2026-01-15 21:38:26 +0530

    c

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-15ec2ecc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e8tkolvj48o"
+    "revision": "0.qulomr5ggb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
